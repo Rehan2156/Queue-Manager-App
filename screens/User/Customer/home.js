c30cba480a4336659d9cfbb3b27783bde58afec8
leo13200006 <laukikchavan101@gmail.com>
2020-06-05 13:53:29 +0530

    Serach Bar is added

--- screens/User/Customer/home.js
+++ screens/User/Customer/home.js
@@ -1,11 +1,10 @@
 import { globalStyles } from '../../../styles/global';
 import Card from '../../../shared/card';
-import Search from '../../../components/searchBar';
 import { MaterialIcons } from '@expo/vector-icons';
 import * as firebase from 'firebase'
 import React, { Component } from 'react'
 import { StyleSheet, View, Text, TouchableOpacity, FlatList,Linking, ActivityIndicator } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SearchBar } from 'react-native-elements';
 
 export default class Home extends Component {
 
@@ -15,7 +14,11 @@ export default class Home extends Component {
               { shopName: 'D Mart', waiting: 30, body: 'lorem ipsum', key: '3',location:'https://goo.gl/maps/wNTPKD9YXLhZGHnx5' },  
            ],
 
+    tempArray: [],
+
     lisIsready: false,
+
+    textToBeSerach: '',
   }
 
   componentDidMount = () => {
@@ -38,15 +41,30 @@ export default class Home extends Component {
           });
           myArray = [...myArray, {shopName: name, body: 'we have to add this field', location: location, key: key, waiting: 10 }]
         })
-        this.setState({
-          shops: [...this.state.shops, ...myArray],
-          lisIsready: true,
-        })
+          this.setState({
+            shops: [...this.state.shops, ...myArray],
+          })
+
+          this.setState({
+            tempArray: this.state.shops,
+            lisIsready: true,
+          })
+
       })
     } catch(e) {
       console.log('Error: ', e)
     }
   }
+
+  searchFilterFunction = text => {    
+    const newData = this.state.shops.filter(item => {      
+      const itemData = `${item.shopName.toString().toUpperCase()}`;
+       const textData = text.toString().toUpperCase();
+       return itemData.indexOf(textData) > -1;    
+    });
+    
+    this.setState({ tempArray: newData });  
+  };
 
   render() {
 
@@ -56,10 +74,22 @@ export default class Home extends Component {
 
     return (
         <View style={globalStyles.container}>
-        <View><Search /></View>  
+        <View style={styles.input}>
+          <SearchBar        
+              placeholder="Search for stores..."        
+              darkTheme        
+              round        
+              onChangeText={text => {
+                this.setState({ textToBeSerach: text })
+                this.searchFilterFunction(text)
+              }}
+              autoCorrect={false}             
+              value = {this.state.textToBeSerach}
+          />    
+        </View>  
         <View>
         <Text style={styles.heading}>Most Recommended</Text>
-        <FlatList data={this.state.shops} renderItem={({ item }) => (
+        <FlatList data={this.state.tempArray} renderItem={({ item }) => (
           <TouchableOpacity style={{backgroundColor:'#F4D03F', borderRadius:10}} onPress={() => this.props.navigation.navigate('ReviewDetails', item)}>
             <Card>
             <View style={styles.cardAlign}>
@@ -81,23 +111,29 @@ export default class Home extends Component {
 }
 
 const styles = StyleSheet.create({
-  heading:{
-      textAlign:'center',
-      borderWidth:1,
-      padding:20,
-      margin:50,
-      fontSize:20,
-      borderRadius:30,
-      backgroundColor:'#AF7AC5',
-      fontFamily:'serif',
-      borderColor:'#AF7AC5',
-      
-  },
-  
-  cardAlign:{
-      flexDirection:'row',
-      justifyContent:'space-between'
-  },
+    heading:{
+        textAlign:'center',
+        borderWidth:1,
+        padding:20,
+        margin:50,
+        fontSize:20,
+        borderRadius:30,
+        backgroundColor:'#AF7AC5',
+        fontFamily:'serif',
+        borderColor:'#AF7AC5',
+        
+    },
+    
+    cardAlign:{
+        flexDirection:'row',
+        justifyContent:'space-between'
+    },
+
+    input:{
+      paddingHorizontal:5,
+      paddingVertical:6
+    }
+
   })
   
 
